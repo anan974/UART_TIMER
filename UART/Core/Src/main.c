f0dcf67a3e694c4b6ebdf4748d3ce427904618f8
Thanh An Dao Dang <22520009@gm.uit.edu.vn>
2024-07-22 16:32:55 +0700

    store

--- UART/Core/Src/main.c
+++ UART/Core/Src/main.c
@@ -29,7 +29,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-// first change
+// again
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
